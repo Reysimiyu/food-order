a3611c12c823db29d4876dbd9b61996f35fc4843
rey <simiyurey@gmail.com>
2023-03-16 11:27:14 +0300

    second commit

--- front-end-partials/footer.php
+++ front-end-partials/footer.php
@@ -3,13 +3,13 @@
         <div class="container">
             <ul >
                 <li>
-                    <a href="#">facebook</a>
+                    <a href="#"><img src="https://img.icons8.com/color/48/null/facebook-new.png"/></a>
                 </li>
                 <li>
-                    <a href="#">Instagram</a>
+                    <a href="#"><img src="https://img.icons8.com/color/48/null/instagram-new--v1.png"/></a>
                 </li>
                 <li>
-                    <a href="#">twitter</a>
+                    <a href="#"><img src="https://img.icons8.com/color/48/null/twitter-circled--v1.png"/></a>
                 </li>
             </ul>
         </div>
