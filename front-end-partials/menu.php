6b84e71875b7c8aadee8b8f30f2ab873d68ba9ae
rey <simiyurey@gmail.com>
2023-03-17 17:56:37 +0300

    first commit

--- front-end-partials/menu.php
+++ front-end-partials/menu.php
@@ -9,7 +9,7 @@ include 'config/db-connect.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>RESTUARANT WEBSITE</title>
     <link rel="stylesheet" href="css/style.css" type="text/css">
     
 
